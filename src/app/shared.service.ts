93b263f861bafc1e7816e8a0cc48e4d380014d62
venkatdev22 <venkat4fz@gmail.com>
2018-07-22 22:16:30 +0530

    Signed-off-by: venkatdev22 <venkat4fz@gmail.com>

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -4,6 +4,12 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {WeatherDetails} from '../app/weather/weatherdetails'; 
 import {MovieDetails} from '../app/movie/moviedetails';
 import {CurrencyDetails} from '../app/currency/currencydetails'; 
+import { catchError, map, tap } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
 
 @Injectable({
   providedIn: 'root'
@@ -21,6 +27,14 @@ export class SharedService {
     this.weatherServiceURL='https://query.yahooapis.com/v1/public/yql?q=select%20name%2C%20country%20from%20geo.places%20where%20text%3D%22'+city+'%2C%20ca%22&diagnostics=true&format=json';
     return this.http.get<WeatherDetails>(this.weatherServiceURL);     
   }   
+
+    /** GET: add a new hero to the server */
+    getWeatherDetails2 (city: String): Observable<WeatherDetails> {
+      return this.http.get<WeatherDetails>(this.weatherServiceURL).pipe(
+        tap((weatherDetails: WeatherDetails) => this.log(`added WeatherDetails w/ id=${weatherDetails.query}`)),
+        catchError(this.handleError<WeatherDetails>('getWeatherDetails'))
+      );
+    }
 
   getCurrencyDetails(cur)
   { 
@@ -40,7 +54,30 @@ export class SharedService {
     return this.http.get<MovieDetails>(this.movieServiceURL);   
   } 
 
+/**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    console.log(`HeroService: ${message}`);
+  }
 
   // messages: string[] = [];
  
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../../app/shared.service';
 import { WeatherDetails } from '../weather/weatherdetails';
+import { Place } from '../weather/weatherdetails';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-weather',
@@ -8,19 +10,34 @@ import { WeatherDetails } from '../weather/weatherdetails';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+ 
+  city:string;
+  country:string;
 
-  weatherDetail: WeatherDetails;
   constructor(public sharedService: SharedService) {
-    sharedService.getWeatherDetails('san%20francisco').subscribe(p => {
-
+    sharedService.getWeatherDetails('san%20francisco').subscribe(p => { 
       //Log weather service API response  
-      console.log(p.query.results.place);
-      this.weatherDetail = p; 
-    });
-
+      // console.log(p.query.results.place);
+      // this.weatherDetail = p; 
+      // this.cityDetail=p.query.results.place
+    }); 
   }
 
   ngOnInit() {
+  }
+
+  onClickWeatherSubmit(city: string)
+  { 
+    //console.log(city); 
+    this.sharedService.getWeatherDetails2('san%20francisco').subscribe(p => { 
+      //Log weather service API response  
+      this.country=p.query.results.place.country.content ; 
+      this.city=p.query.results.place.name;
+    });  
+  }
+
+  getHeroes(city: string): Observable<string> { 
+    return of(this.city);
   }
 
 }
